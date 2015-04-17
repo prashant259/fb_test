320c4d17dbfea99f0e3fcf838ea28570588397a8
[Prashant] <[garimella.prashant@gmail.com]>
2015-04-17 00:13:43 -0700

    Updated the drop api

--- tst_drop.py
+++ tst_drop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import dropbox 
+import pprint
 
 from dropbox.client import DropboxOAuth2FlowNoRedirect, DropboxClient
 from dropbox import rest as dbrest
@@ -11,23 +12,25 @@ app_secret = 'a10tg8xu9guwbox'
 
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
-# Have the user sign in and authorize this token
-authorize_url = flow.start()
-print '1. Go to: ' + authorize_url
-print '2. Click "Allow" (you might have to log in first)'
-print '3. Copy the authorization code.'
-code = raw_input("Enter the authorization code here: ").strip()
+try: 
+    client = dropbox.client.DropboxClient(access_token)  
+    folder_metadata = client.metadata('/')
+except Exception, e:
 
-# This will fail if the user enters an invalid authorization code
-access_token, user_id = flow.finish(code)
+    print "No previous authentication, Aunthenticate now !!" 
+    # Have the user sign in and authorize this token
+    authorize_url = flow.start()
+    print '1. Go to: ' + authorize_url
+    print '2. Click "Allow" (you might have to log in first)'
+    print '3. Copy the authorization code.'
+    code = raw_input("Enter the authorization code here: ").strip()
+    ## This will fail if the user enters an invalid authorization code
+    access_token, user_id = flow.finish(code)
+    ## After obtaining access_token, accessing client data
+    client = dropbox.client.DropboxClient(access_token)
 
-client = dropbox.client.DropboxClient(access_token)
-# dump user account info.
-#print 'linked account: '
-#for X in client.account_info():
-#    print X
-
-# dump folder metadata
+### dump folder metadata
 folder_metadata = client.metadata('/')
-for Y in folder_metadata:
-    print Y
+## Setting pretty print option
+pp = pprint.PrettyPrinter(indent=4)
+print "\n metadata: " + pp.pprint(folder_metadata)
